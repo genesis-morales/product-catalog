0d503386fbe01e09826cd1c829a31378bb8d001b
Génesis Morales <genesismoralesc1@gmail.com>
2026-06-10 13:34:26 -0600

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -16,11 +16,6 @@ use App\Http\Controllers\Api\AdminCustomerController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/', function () { return response()->json([
-        'name' => 'Product Catalog API',
-        'status' => 'ok'
-    ]);
-});
 
 // Rutas públicas
 Route::post('/register', [AuthController::class, 'register']);
